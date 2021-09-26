12b2f688062ef7dd9d3960be6f4c34c8eb3477b5
d-ava <d.avalishvili@gmail.com>
2021-09-26 20:19:16 +0400

    3

--- app/src/main/java/com/example/shemajamebeli2/MainActivity.kt
+++ app/src/main/java/com/example/shemajamebeli2/MainActivity.kt
@@ -13,94 +13,126 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // binding = ActivityMainBinding.inflate(layoutInflater)
+        // binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val userMap = mutableMapOf<String, String>()
-
-        binding.btnAddUser.setOnClickListener {
-            val email:String = binding.ETeMail.text.toString()
-            val name:String = binding.ETName.text.toString()
-
-            userMap[email] = name
-
-            binding.additionalTextInfo.text = "map $userMap"
-        }
-
-    /*    fun infoTXT(text:String, redGreen:Int){
-            if (redGreen == 0){
-                binding.textInfo.setTextColor(Color.RED)
-                binding.textInfo.text = text
-            }else{
-                binding.textInfo.setTextColor(Color.GREEN)
-                binding.textInfo.text = text
-            }
-
-        }
-
-
-        val email:String = binding.ETeMail.text.toString()
-        val name = binding.ETName.text.toString()
-        val age: String = binding.ETage.text.toString()
-        val lastName = binding.ETlastName.text.toString()
-
 
         val userMap = mutableMapOf<String, List<String>>()
 
+        binding.btnAddUser.setOnClickListener {
+            val email: String = binding.ETeMail.text.toString()
+            val name: String = binding.ETName.text.toString()
+            val age: String = binding.ETage.text.toString()
+            val lastName = binding.ETlastName.text.toString()
 
-        fun addUser() {
-
-            if (email in userMap.keys) {
-                infoTXT("user is already here",0)
-            } else {
+            if (email !in userMap.keys) {
 
                 userMap[email] = listOf<String>(name, lastName, age)
-                infoTXT("user added",1)
-                    // binding.additionalTextInfo.text = "map $userMap"
+
+                binding.additionalTextInfo.text = "userlist $userMap"
+                binding.textInfo.setTextColor(Color.GREEN)
+                binding.textInfo.text = "USER ADDED"
+            } else {
+
+                binding.textInfo.setTextColor(Color.RED)
+                binding.textInfo.text = "USER is already here"
             }
-
-        }
-        binding.btnAddUser.setOnClickListener {
-            addUser()
         }
 
-
-
-
-        fun removeUser() {
+        binding.btnRemoveUser.setOnClickListener {
+            val email: String = binding.ETeMail.text.toString()
 
             if (email in userMap.keys) {
                 userMap.remove(email)
-                infoTXT("bye bye ${userMap[email]}",1)
+                binding.textInfo.setTextColor(Color.GREEN)
+                binding.textInfo.text = "USER removed"
+                binding.additionalTextInfo.text = "userlist $userMap"
+
             } else {
-                infoTXT("no such user",0)
+                binding.textInfo.setTextColor(Color.RED)
+                binding.textInfo.text = "nu such user$email"
 
             }
+
+
         }
 
-        fun updateUser(){
-            if (email in userMap.keys){
-                userMap[email]= listOf<String>(name, lastName, age)
-                infoTXT("user $email has been updated",1)
-            }else{
-                infoTXT("no such user",0)
+
+
+
+        /*    fun infoTXT(text:String, redGreen:Int){
+                if (redGreen == 0){
+                    binding.textInfo.setTextColor(Color.RED)
+                    binding.textInfo.text = text
+                }else{
+                    binding.textInfo.setTextColor(Color.GREEN)
+                    binding.textInfo.text = text
+                }
+
             }
-        }
+
+
+            val email:String = binding.ETeMail.text.toString()
+            val name = binding.ETName.text.toString()
+            val age: String = binding.ETage.text.toString()
+            val lastName = binding.ETlastName.text.toString()
+
+
+            val userMap = mutableMapOf<String, List<String>>()
+
+
+            fun addUser() {
+
+                if (email in userMap.keys) {
+                    infoTXT("user is already here",0)
+                } else {
+
+                    userMap[email] = listOf<String>(name, lastName, age)
+                    infoTXT("user added",1)
+                        // binding.additionalTextInfo.text = "map $userMap"
+                }
+
+            }
+            binding.btnAddUser.setOnClickListener {
+                addUser()
+            }
+
+
+
+
+            fun removeUser() {
+
+                if (email in userMap.keys) {
+                    userMap.remove(email)
+                    infoTXT("bye bye ${userMap[email]}",1)
+                } else {
+                    infoTXT("no such user",0)
+
+                }
+            }
+
+            fun updateUser(){
+                if (email in userMap.keys){
+                    userMap[email]= listOf<String>(name, lastName, age)
+                    infoTXT("user $email has been updated",1)
+                }else{
+                    infoTXT("no such user",0)
+                }
+            }
 
 
 
 
 
 
-        binding.btnRemoveUser.setOnClickListener {
-            removeUser()
+            binding.btnRemoveUser.setOnClickListener {
+                removeUser()
 
-        }
+            }
 
-        binding.btnUpdateUser.setOnClickListener {
-            updateUser()
-        }
-*/
+            binding.btnUpdateUser.setOnClickListener {
+                updateUser()
+            }
+    */
 
     }
 }
