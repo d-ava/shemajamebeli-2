1f6c5eb84eb528e336a504304cfd374e0283bc9f
d-ava <d.avalishvili@gmail.com>
2021-09-28 08:39:15 +0400

    UserData class created.
    on btn add user click for loop to check if email field is unique and add UserData to the list.

--- app/src/main/java/com/example/shemajamebeli2/MainActivity.kt
+++ app/src/main/java/com/example/shemajamebeli2/MainActivity.kt
@@ -3,28 +3,42 @@ package com.example.shemajamebeli2
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.shemajamebeli2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    //private lateinit var binding: ActivityMainBinding
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
-        val userMap = mutableMapOf<String, List<String>>()
+        //val userMap = mutableMapOf<String, List<String>>()
+
+        var list = mutableListOf<UserData>()
 
         binding.btnAddUser.setOnClickListener {
             val email: String = binding.ETeMail.text.toString()
             val name: String = binding.ETName.text.toString()
-            val age: String = binding.ETage.text.toString()
+            val age: Int = binding.ETage.text.toString().toInt()
             val lastName = binding.ETlastName.text.toString()
 
-            if (email !in userMap.keys) {
+            //var userData:UserData = UserData(email, name, age, lastName)
+
+            for (element in list){
+                if (element.email == email){
+                    Toast.makeText(this, "email already", Toast.LENGTH_LONG).show()
+                }
+                else list.add(UserData(email, name, age, lastName))
+            }
+
+            list.add(UserData(email, name, age, lastName))
+
+
+            /*if (email !in userMap.keys) {
 
                 userMap[email] = listOf<String>(name, lastName, age)
 
@@ -36,43 +50,43 @@ class MainActivity : AppCompatActivity() {
                 binding.textInfo.setTextColor(Color.RED)
                 binding.textInfo.text = "USER is already here"
             }
-        }
+        }*/
 
-        binding.btnRemoveUser.setOnClickListener {
-            val email: String = binding.ETeMail.text.toString()
+            binding.btnRemoveUser.setOnClickListener {
+                /*val email: String = binding.ETeMail.text.toString()
 
-            if (email in userMap.keys) {
-                userMap.remove(email)
-                binding.textInfo.setTextColor(Color.GREEN)
-                binding.textInfo.text = "USER removed"
-                binding.additionalTextInfo.text = "userlist $userMap"
+                if (email in userMap.keys) {
+                    userMap.remove(email)
+                    binding.textInfo.setTextColor(Color.GREEN)
+                    binding.textInfo.text = "USER removed"
+                    binding.additionalTextInfo.text = "userlist $userMap"
 
-            } else {
-                binding.textInfo.setTextColor(Color.RED)
-                binding.textInfo.text = "nu such user$email"
+                } else {
+                    binding.textInfo.setTextColor(Color.RED)
+                    binding.textInfo.text = "nu such user$email"
+
+                }*/
+
 
             }
 
+            /*binding.btnUpdateUser.setOnClickListener {
+                val email: String = binding.ETeMail.text.toString()
+                val name: String = binding.ETName.text.toString()
+                val age: String = binding.ETage.text.toString()
+                val lastName = binding.ETlastName.text.toString()
 
+                if (email in userMap.keys){
+                    userMap[email]= listOf<String>(name, lastName, age)
+                    binding.textInfo.setTextColor(Color.GREEN)
+                    binding.textInfo.text = "USER updated"
+                    binding.additionalTextInfo.text = "userlist $userMap"
+                }else{
+                    binding.textInfo.setTextColor(Color.RED)
+                    binding.textInfo.text = "nu such user$email"
+                }
+
+            */
         }
-
-        binding.btnUpdateUser.setOnClickListener {
-            val email: String = binding.ETeMail.text.toString()
-            val name: String = binding.ETName.text.toString()
-            val age: String = binding.ETage.text.toString()
-            val lastName = binding.ETlastName.text.toString()
-
-            if (email in userMap.keys){
-                userMap[email]= listOf<String>(name, lastName, age)
-                binding.textInfo.setTextColor(Color.GREEN)
-                binding.textInfo.text = "USER updated"
-                binding.additionalTextInfo.text = "userlist $userMap"
-            }else{
-                binding.textInfo.setTextColor(Color.RED)
-                binding.textInfo.text = "nu such user$email"
-            }
-
-        }
-
     }
 }
---
+++ app/src/main/java/com/example/shemajamebeli2/UserData.kt
@@ -0,0 +1,9 @@
+package com.example.shemajamebeli2
+
+data class UserData(
+    var email:String = "",
+    var name:String = "",
+    var age:Int = 0,
+    var lastName:String = ""
+
+)
